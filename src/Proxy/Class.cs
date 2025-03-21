fe0d422bad74622619558352e63c280fa5bc0104
Jeppe Ravn <jepperc@gmail.com>
2025-03-22 00:05:48 +0100

    refactor auth

---
+++ src/Proxy/Class.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpProxy
+{
+    using System.Text.Json.Serialization;
+
+    public class AuthResponse
+    {
+        [JsonPropertyName("session")]
+        public required Session Session { get; set; }
+    }
+
+    public class Session
+    {
+        [JsonPropertyName("valid")]
+        public required bool Valid { get; set; }
+
+        [JsonPropertyName("totp")]
+        public required bool Totp { get; set; }
+
+        [JsonPropertyName("sid")]
+        public required string Sid { get; set; }
+
+        [JsonPropertyName("csrf")]
+        public required string Csrf { get; set; }
+
+        [JsonPropertyName("validity")]
+        public required int Validity { get; set; }
+
+        [JsonPropertyName("message")]
+        public required string Message { get; set; }
+    }
+
+}
